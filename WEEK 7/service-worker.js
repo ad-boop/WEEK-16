7d3d1d9ef432dd2f1dcc5e7188eb991a1eba8d64
Adora F <AF908@live.mdx.ac.uk>
2023-02-07 10:23:56 +0400

    hey

--- WEEK 7/service-worker.js
+++ WEEK 7/service-worker.js
@@ -20,3 +20,28 @@ self.addEventListener('install',(e)=>{
         })
     )
 })
+
+// self.addEventListener('fetch',function(e){
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function(r){
+//             console.log('[SERVICE WORKER] FETCHING RESOURCE:'+e.request.url);
+//             // r is the matching file if it exists in the cache
+//             return r
+//         })
+//     )
+// })
+
+
+self.addEventListener('fetch',function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(r){
+            return r || fetch(e.request).then(function(response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request,response.clone());
+                    return response;
+                })
+            })
+        })
+    )
+})
